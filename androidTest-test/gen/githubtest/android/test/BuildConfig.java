68166a2fc887fea6da3e90cbf481b9314e7f4f41
hughabbott <hugh.abbott@gmail.com>
2013-01-31 10:57:54 +0000

    creating a repository full of android files

---
+++ androidTest-test/gen/githubtest/android/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package githubtest.android.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ androidTest/gen/githubtest/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package githubtest.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
